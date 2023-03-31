21770e50791be9719e77bc0f630aa1556db6cf47
Hepheir <hepheir@gmail.com>
2023-03-31 10:59:51 +0900

    fix: EduIntro 라이브러리를 이용하여 DHT 실습코드가 동작하도록 수정

--- 04 센서 제어하기/02 디지털 온습도센서/스케치 4-2 10.DHT11 스케치.ino
+++ 04 센서 제어하기/02 디지털 온습도센서/스케치 4-2 10.DHT11 스케치.ino
@@ -1,16 +1,30 @@
-#include "DHT.h
+/*
+  Author: 202115064 김동주 <hepheir@gmail.com>
+
+  Prerequisite:
+    (Library) EduIntro by Arduino LLC (v0.0.16)
+    -> can be installed via Arduino IDE Library manager
+*/
+
+#include <EduIntro.h>
+
 #define DHTPIN 2
 #define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+
+DHT11 dht(DHTPIN);
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
+    // EduIntro: read data and return status
+    dht.update();
+
     delay(2000);
     int h = dht.readHumidity();
-    int t = dht.readTemperature();
+    int t = dht.readCelsius();
+
     Serial.print("Humidity: ");
     Serial.print(h);
     Serial.print(" %\t");
